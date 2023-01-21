6e9aefaa81ee80b793e8b3d1806a558a98f7ecb6
osmandgn <oskan001@gmail.com>
2023-01-21 20:29:40 +0300

    V3

--- src/main/java/com/dgn/domain/Review.java
+++ src/main/java/com/dgn/domain/Review.java
@@ -1,4 +1,4 @@
-package com.dgn.movies;
+package com.dgn.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/test/java/com/dgn/MoviesApplicationTests.java
+++ src/test/java/com/dgn/MoviesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dgn.movies;
+package com.dgn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
